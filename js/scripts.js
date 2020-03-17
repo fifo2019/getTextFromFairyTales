cfa86972fa4908e5ad8196df8235bb8ef97194f4
pavel <pavel.dudkov@icloud.com>
2020-03-17 20:53:06 +0400

    Homework done

--- js/scripts.js
+++ js/scripts.js
@@ -7,17 +7,37 @@ function appendText(data) {
 
     textBox += `</p>`;
 
+    textBox = textBox.replace(/{/g, "<span class='text-update'>{");
+    textBox = textBox.replace(/}/g, "}</span>");
+
     $(textBox).appendTo('.text-wrp');
 }
 
 
 $(function() {
-    $('.js-create-btn').click(function () {
+    let $body = $('body');
+
+    $body.on('click', '.js-create-btn', function () {
         $.getJSON("https://api.myjson.com/bins/jcmhn", function (data) {
-            $('.js-create-btn').fadeOut(400);
+            $('.text-create-wrp').fadeOut(200);
             setTimeout(function () {
                 appendText(data);
-            }, 600);
+                $('.text-processing').css("display", "flex");
+                $('.text-create-wrp').remove()
+            }, 200);
         });
+    });
+
+
+    $body.on('click', '.js-text-update-btn', function () {
+        let $selectedElement = $(this).prev();
+        let nameElement = $selectedElement[0].name;
+        let valueElement = $selectedElement[0].value;
+
+        $('.text-update').each(function () {
+            if (this.innerHTML === `{${nameElement}}`) {
+                this.innerHTML = valueElement;
+            }
+        })
     })
 });
